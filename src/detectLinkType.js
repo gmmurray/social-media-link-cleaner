3aae89b02698d917bf9b9f5a91dc8f8bd7a8d87a
gmmurray <trinilime123@live.com>
2021-11-08 23:02:51 -0500

    added tests for cleaning algorithm

--- src/detectLinkType.js
+++ src/detectLinkType.js
@@ -1,4 +1,26 @@
+export const linkTypes = {
+    tiktokMobile: {
+        url: 'vm.tiktok.com',
+        name: 'TikTok Mobile',
+    },
+    tiktokWeb: {
+        url: 'tiktok.com',
+        name: 'Tik Tok Web',
+    },
+    instagram: {
+        url: 'instagram.com',
+        name: 'Instagram',
+    },
+};
+
 export const detectLinkType = link => {
     if (!link || typeof link !== 'string' || link.replace(/ /g, '') === '')
         return null;
+
+    const { tiktokMobile, tiktokWeb, instagram } = linkTypes;
+
+    if (link.includes(tiktokMobile.url)) return tiktokMobile.name;
+    else if (link.includes(tiktokWeb.url)) return tiktokWeb.name;
+    else if (link.includes(instagram.url)) return instagram.name;
+    else return null;
 };
--- src/detectLinkType.test.js
+++ src/detectLinkType.test.js
@@ -1,4 +1,4 @@
-import { detectLinkType } from './detectLinkType';
+import { detectLinkType, linkTypes } from './detectLinkType';
 
 describe(detectLinkType.name, () => {
     it('handles null input', () => {
@@ -24,5 +24,25 @@ describe(detectLinkType.name, () => {
     it('handles empty whitespace input', () => {
         const result = detectLinkType('  ');
         expect(result).toBe(null);
+    });
+
+    it('handles valid but unsupported link types', () => {
+        const result = detectLinkType('facebook.com');
+        expect(result).toBe(null);
+    });
+
+    it('detects mobile tiktok links', () => {
+        const result = detectLinkType('https://vm.tiktok.com/randomstuff');
+        expect(result).toBe(linkTypes.tiktokMobile.name);
+    });
+
+    it('detects web tiktok links', () => {
+        const result = detectLinkType('https://tiktok.com/randomstuff');
+        expect(result).toBe(linkTypes.tiktokWeb.name);
+    });
+
+    it('detects instagram links', () => {
+        const result = detectLinkType('https://instagram.com/randomstuff');
+        expect(result).toBe(linkTypes.instagram.name);
     });
 });
